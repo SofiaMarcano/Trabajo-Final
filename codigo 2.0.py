c07930de7392b9081c01929848fe952d702e091e
SofiaMarcano <sofia.marcano@udea.edu.co>
2024-12-06 22:31:43 -0500

    codigo 2.0.py

--- codigo 2.0.py
+++ codigo 2.0.py
@@ -1,146 +1,221 @@
 import functions
+db_config = {
+    "host": "localhost",
+    "user": "informatica1",
+    "password": "info20242",
+    "database":"SistemaMedico"
+}
+functions.iny_mysql()
 while True:
-    msj=("Bienvenido. \nSeleccione una opción. \n1.Ingresar al sistema. \n3.Ingresar al submenú \n3.Salir. \n===============\n")
-    op=functions.rev_num(msj)
-    if op==1:
-        rol=functions.login
-        if rol=="administrador":
-            msj=("Bienvenido al menú administradir. \nSeleccione una opción: \n1.Gestión de usuario. \n2.Gestión de pacientes. \n3.Gestión de Diagnósticos. \n4.Gestión de imágenes. \n5.Salir \n======================\n")
-            opcion=functions.ver_num(msj)
-            while True:
-                if opcion==1:
-                    while True:
-                        msj=("Ingrese una opción: \n1.Añadir usuario. \n2.Modificar usuario. \n3.Eliminar usuario. \n4.Salir. \n=================\n")
-                        opcion=functions.ver_num(msj)
-                        if opcion==1:
-                            functions.create_user()
-                        elif opcion==2:
-                            functions.mod_user()
-                        elif opcion==3:
-                            functions.del_user()
-                        elif opcion==4:
-                            break
-                        else:
-                            print("Ingrese una opción válida.")
-                            continue
-                elif opcion==2:
-                    while True:
-                        msj=("Ingrese una opción: \n1.Crear paciente. \n2.Leer paciente. \n3.Actualizar paciente. \n4.Eliminar paciente. \n5.Salir. \n=================\n")
-                        opcion=functions.ver_num(msj)
-                        if opcion==1:
-                            functions.create_pac()
-                        elif opcion==2:
-                            functions.read_pac()
-                        elif opcion==3:
-                            functions.act_pac()
-                        elif opcion==4:
-                            functions.del_pac()
-                        elif opcion==5:
-                            break
-                        else:
-                            print("Ingrese una opción válida.")
-                            continue
-                elif opcion==3:
-                    while True:
-                        msj=("Ingrese una opción: \n1.Registrar diagnóstico. \n2.Actualizar diagnóstico. \n3.Ver diagnóstico. \n4.Eliminar diagnóstico. \n5.Salir \n=================\n")
-                        opcion=functions.ver_num(msj)
-                        if opcion==1:
-                            functions.create_diag()
-                        elif opcion==2:
-                            functions.act_diag()
-                        elif opcion==3:
-                            functions.read_diag()
-                        elif opcion==4:
-                            functions.del_diag()
-                        elif opcion==5:
-                            break
-                        else:
-                            print("Ingrese una opción válida.")
-                            continue
-                elif opcion==4:
-                    while True:
-                        msj=("Ingrese una opción: \n1.Almacenar imagen. \n2.Ver imagen. \n3.Eliminar imagen. \n4.Salir \n=================\n")
-                        opcion=functions.ver_num(msj)
-                        if opcion==1:
-                            functions.alm_imagenes()
-                        elif opcion==2:
-                            functions.ver_imagen()
-                        elif opcion==3:
-                            functions.eliminar_imagen()
-                        elif opcion==4:
-                            break
-                elif opcion==5:
-                    break
-                else:
-                    print("Ingrese una opción válida.")
-                    continue
-        elif rol=="medico":
-            msj=("Bienvenido al menú medico. \nSeleccione una opción: \n1.Gestionar pacientes \n2.Gestinar diagnósticos. \n3.Gestionar reportes médicos. \n4.Salir. \n============\n")
-            opcion=functions.ver_num(msj)
-            while True:
-                if opcion==1:
-                    while True:
-                        msj=("Ingrese una opción: \n1.Leer paciente. \n2.Actualizar paciente. \n3.Salir. \n=================\n")
-                        opcion=functions.ver_num(msj)
-                        if opcion==1:
-                            functions.read_pac()
-                        elif opcion==2:
-                            functions.act_pac()
-                        elif opcion==3:
-                            break
-                        else:
-                            print("Ingrese una opción válida.")
-                            continue
-                elif opcion==2:
-                    while True:
-                        msj=("Ingrese una opción: \n1.Actualizar diagnóstico. \n2.Ver diagnóstico. \n3.Salir \n=================\n")
-                        opcion=functions.ver_num(msj)
-                        if opcion==1:
-                            functions.act_diag()
-                        elif opcion==2:
-                            functions.read_diag()
-                        elif opcion==3:
-                            break
-                        else:
-                            print("Ingrese una opción válida.")
-                            continue
-                elif opcion==3:
-                    while True:
-                        msj=("Ingrese una opción: \n1.Crear reporte. \n2.Actualizar reporte. \n3.Salir \n=================\n")
-                        opcion=functions.ver_num(msj)
-                        if opcion==1:
-                            functions.create_reporte()
-                        elif opcion==2:
-                            functions.act_reporte()
-                        elif opcion==3:
-                            break
-                        else:
-                            print("Ingrese una opción válida.")
-                            continue
-                elif opcion==4:
-                    break
-                else:
-                    print("Ingrese una opción válida.")
-                    continue
-        elif rol=="tecnico":
-            msj=("Bienvenido al menú tecnico. \nSeleccione una opción: \n1.Añadir imágenes. \n2.Añadir notas técnicas. \n3.Salir. \n===============\n")
-            opcion=functions.ver_num(msj)
-            while True:
-                if opcion==1:
-                    functions.alm_imagenes()
-                elif opcion==2:
-                    functions.add_nota_tec()
-                elif opcion==3:
-                    break
-                else:
-                    print("Ingrese una opción válida.")
-                    continue
-    elif op==4:
-        msj=("Ingrese el ID del paciente: \n")
-        patient_id=functions.rev_num(msj)
-        functions.search_pac(patient_id)
-    elif op==3:
+    print("Por favor, inicie sesión.")
+    user_data = functions.login()  
+    if user_data:
+        user, rol = user_data
+        print(f"Bienvenido, {user}. Tu rol es: {rol}.")
         break
     else:
-        print("Ingrese una opción válida")
-        continue
+        print("Login fallido. Por favor, intente nuevamente.")
+while True:
+    if rol == "administrador":
+        msj = (
+            f"Bienvenido al menú administrador, {user}.\n"
+            "Seleccione una opción:\n"
+            "1. Gestión de usuario.\n"
+            "2. Gestión de pacientes.\n"
+            "3. Gestión de diagnósticos.\n"
+            "4. Gestión de imágenes.\n"
+            "5. Salir.\n"
+            "======================\n"
+        )
+        opc= functions.ver_num(msj)
+        if opc== 1:
+            while True:
+                msj = (
+                    "Ingrese una opción:\n"
+                    "1. Añadir usuario.\n"
+                    "2. Modificar usuario.\n"
+                    "3. Eliminar usuario.\n"
+                    "4. Salir.\n"
+                    "=================\n"
+                )
+                opc1 = functions.rev_num(msj)
+                if opc1 == 1:
+                    functions.create_user()
+                elif opc1 == 2:
+                    functions.mod_user()
+                elif opc1 == 3:
+                    functions.del_user()
+                elif opc1 == 4:
+                    break
+                else:
+                    print("Ingrese una opción válida.")
+        elif opc == 2:
+            while True:
+                msj = (
+                    "Ingrese una opción:\n"
+                    "1. Crear paciente.\n"
+                    "2. Leer paciente.\n"
+                    "3. Actualizar paciente.\n"
+                    "4. Eliminar paciente.\n"
+                    "5. Salir.\n"
+                    "=================\n"
+                )
+                opc1 = functions.rev_num(msj)
+                if opc1 == 1:
+                    functions.create_pac()
+                elif opc1== 2:
+                    functions.read_pac()
+                elif opc1== 3:
+                    functions.act_pac()
+                elif opc1== 4:
+                    functions.del_pac()
+                elif opc1== 5:
+                    break
+                else:
+                    print("Ingrese una opción válida.")
+        elif opc== 3:
+            while True:
+                msj = (
+                    "Ingrese una opción:\n"
+                    "1. Registrar diagnóstico.\n"
+                    "2. Actualizar diagnóstico.\n"
+                    "3. Ver diagnóstico.\n"
+                    "4. Eliminar diagnóstico.\n"
+                    "5. Salir.\n"
+                    "=================\n"
+                )
+                opc1 = functions.ver_num(msj)
+                if opc1 == 1:
+                    functions.create_diag()
+                elif opc1 == 2:
+                    functions.act_diag()
+                elif opc1== 3:
+                    functions.read_diag()
+                elif opc1== 4:
+                    functions.del_diag()
+                elif opc1== 5:
+                    break
+                else:
+                    print("Ingrese una opción válida.")
+        elif opc== 4:
+            while True:
+                msj = (
+                    "Ingrese una opción:\n"
+                    "1. Almacenar imagen.\n"
+                    "2. Ver imagen.\n"
+                    "3. Eliminar imagen.\n"
+                    "4. Salir.\n"
+                    "=================\n"
+                )
+                opc1= functions.ver_num(msj)
+                if opc1== 1:
+                    functions.alm_imagenes()
+                elif opc1== 2:
+                    functions.ver_imagen()
+                elif opc1== 3:
+                    functions.eliminar_imagen()
+                elif opc1== 4:
+                    break
+                else:
+                    print("Ingrese una opción válida.")
+        elif opc== 5:
+            print("Saliendo del menú administrador...")
+            break
+        else:
+            print("Ingrese una opción válida.")
+    
+    elif rol == "medico":
+        msj = (
+            f"Bienvenido al menú médico, {user}.\n"
+            "Seleccione una opción:\n"
+            "1. Gestionar pacientes.\n"
+            "2. Gestionar diagnósticos.\n"
+            "3. Gestionar reportes médicos.\n"
+            "4. Salir.\n"
+            "============\n"
+        )
+        opc = functions.ver_num(msj)
+        if opc == 1:
+            while True:
+                msj = (
+                    "Ingrese una opción:\n"
+                    "1. Leer paciente.\n"
+                    "2. Actualizar paciente.\n"
+                    "3. Salir.\n"
+                    "=================\n"
+                )
+                opc1 = functions.ver_num(msj)
+                if opc1 == 1:
+                    functions.read_pac()
+                elif opc1 == 2:
+                    functions.act_pac()
+                elif opc1 == 3:
+                    break
+                else:
+                    print("Ingrese una opción válida.")
+        elif opc == 2:
+            while True:
+                msj = (
+                    "Ingrese una opción:\n"
+                    "1. Actualizar diagnóstico.\n"
+                    "2. Ver diagnóstico.\n"
+                    "3. Salir.\n"
+                    "=================\n"
+                )
+                opc1 = functions.ver_num(msj)
+                if opc1 == 1:
+                    functions.act_diag()
+                elif opc1 == 2:
+                    functions.read_diag()
+                elif opc1 == 3:
+                    break
+                else:
+                    print("Ingrese una opción válida.")
+        elif opc == 3:
+            while True:
+                msj = (
+                    "Ingrese una opción:\n"
+                    "1. Crear reporte.\n"
+                    "2. Actualizar reporte.\n"
+                    "3. Salir.\n"
+                    "=================\n"
+                )
+                opc1 = functions.ver_num(msj)
+                if opc1 == 1:
+                    functions.create_reporte()
+                elif opc1 == 2:
+                    functions.act_reporte()
+                elif opc1 == 3:
+                    break
+                else:
+                    print("Ingrese una opción válida.")
+        elif opc == 4:
+            print("Saliendo del menú médico...")
+            break
+        else:
+            print("Ingrese una opción válida.")
+    
+    elif rol == "tecnico":
+        msj = (
+            f"Bienvenido al menú técnico, {user}.\n"
+            "Seleccione una opción:\n"
+            "1. Añadir imágenes.\n"
+            "2. Añadir notas técnicas.\n"
+            "3. Salir.\n"
+            "===============\n"
+        )
+        opc1 = functions.ver_num(msj)
+        if opc1 == 1:
+            functions.alm_imagenes()
+        elif opc1== 2:
+            functions.add_nota_tec()
+        elif opc1 == 3:
+            print("Saliendo del menú técnico...")
+            break
+        else:
+            print("Ingrese una opción válida.")
+    
+    else:
+        print("Rol no reconocido. Saliendo del sistema...")
+        break
+
